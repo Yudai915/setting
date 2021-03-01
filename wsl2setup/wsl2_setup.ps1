046b19f0f13e4d3afe13db5083c26ab6736a5a08
Romira915 <40430090+Romira915@users.noreply.github.com>
2021-03-01 18:22:07 +0900

    pathfix

--- wsl2setup/wsl2_setup.ps1
+++ wsl2setup/wsl2_setup.ps1
@@ -1,7 +1,7 @@
 Set-Location (Split-Path $script:myInvocation.MyCommand.path -parent)
 dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
 dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
-$path = Resolve-Path ..\wsl2setup\wsl2_after-reboot.ps1
+$path = Resolve-Path .\wsl2_after-reboot.ps1
 $commnd = 'powershell -ExecutionPolicy Unrestricted -File' + $path
 New-ItemProperty -LiteralPath 'HKCU:Software\Microsoft\Windows\CurrentVersion\RunOnce' -Name 'wsl2_after-reboot' -PropertyType 'String' -Value $commnd -force
 Restart-Computer
