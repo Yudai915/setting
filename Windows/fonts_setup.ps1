0e2c876409de025adc1b8d11b429608fe5222316
Romira915 <40430090+Romira915@users.noreply.github.com>
2021-04-09 01:56:29 +0900

    font fix and wt setting file fix

--- Windows/fonts_setup.ps1
+++ Windows/fonts_setup.ps1
@@ -1,6 +1,7 @@
 Set-Location (Split-Path $script:myInvocation.MyCommand.path -parent)
 
 git clone https://github.com/mzyy94/RictyDiminished-for-Powerline.git
-git clone https://github.com/microsoft/cascadia-code.git
+Invoke-WebRequest -UseBasicParsing -Uri https://github.com/microsoft/cascadia-code/releases/download/v2102.25/CascadiaCode-2102.25.zip -OutFile CascadiaCode-2102.25.zip
+
 
 # Fonts install
